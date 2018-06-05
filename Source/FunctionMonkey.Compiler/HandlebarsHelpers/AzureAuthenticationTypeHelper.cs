8748ac56d3c8966198302ae6c90c6cc666ec887d
James Randall <james@accidentalfish.com>
2018-06-05 09:13:38 +0100

    Fixed json authentication

--- Source/FunctionMonkey.Compiler/HandlebarsHelpers/AzureAuthenticationTypeHelper.cs
+++ Source/FunctionMonkey.Compiler/HandlebarsHelpers/AzureAuthenticationTypeHelper.cs
@@ -8,6 +8,12 @@ namespace FunctionMonkey.Compiler.HandlebarsHelpers
     {
         public static void Register()
         {
+            RegisterCSharpAuthenticationType();
+            RegisterJsonAuthenticationType();
+        }
+
+        private static void RegisterCSharpAuthenticationType()
+        {
             Handlebars.RegisterHelper("azureAuthenticationType", (writer, context, parameters) =>
             {
                 if (context is HttpFunctionDefinition httpFunctionDefinition)
@@ -19,7 +25,8 @@ namespace FunctionMonkey.Compiler.HandlebarsHelpers
                     else
                     {
                         // if we're using token validation then we use an anonymous function, it's secured internally with the token support
-                        if (httpFunctionDefinition.Authorization == AuthorizationTypeEnum.Anonymous || httpFunctionDefinition.Authorization == AuthorizationTypeEnum.TokenValidation)
+                        if (httpFunctionDefinition.Authorization == AuthorizationTypeEnum.Anonymous ||
+                            httpFunctionDefinition.Authorization == AuthorizationTypeEnum.TokenValidation)
                         {
                             writer.Write("AuthorizationLevel.Anonymous");
                         }
@@ -29,14 +36,53 @@ namespace FunctionMonkey.Compiler.HandlebarsHelpers
                         }
                         else
                         {
-                            throw new CompilerException($"Authorization type {httpFunctionDefinition.Authorization} can not be translated to a HTTP function");
+                            throw new CompilerException(
+                                $"Authorization type {httpFunctionDefinition.Authorization} can not be translated to a HTTP function");
                         }
                     }
                 }
                 else
                 {
-                    throw new CompilerException("azureAuthenticationType helper can only be used with a HttpFunctionDefinition");
-                }                
+                    throw new CompilerException(
+                        "azureAuthenticationType helper can only be used with a HttpFunctionDefinition");
+                }
+            });
+        }
+
+        private static void RegisterJsonAuthenticationType()
+        {
+            Handlebars.RegisterHelper("jsonAuthenticationType", (writer, context, parameters) =>
+            {
+                if (context is HttpFunctionDefinition httpFunctionDefinition)
+                {
+                    if (!httpFunctionDefinition.Authorization.HasValue)
+                    {
+                        writer.Write("function");
+                    }
+                    else
+                    {
+                        // if we're using token validation then we use an anonymous function, it's secured internally with the token support
+                        if (httpFunctionDefinition.Authorization == AuthorizationTypeEnum.Anonymous ||
+                            httpFunctionDefinition.Authorization == AuthorizationTypeEnum.TokenValidation)
+                        {
+                            writer.Write("anonymous");
+                        }
+                        else if (httpFunctionDefinition.Authorization == AuthorizationTypeEnum.Function)
+                        {
+                            writer.Write("function");
+                        }
+                        else
+                        {
+                            throw new CompilerException(
+                                $"Authorization type {httpFunctionDefinition.Authorization} can not be translated to a HTTP function");
+                        }
+                    }
+                }
+                else
+                {
+                    throw new CompilerException(
+                        "azureAuthenticationType helper can only be used with a HttpFunctionDefinition");
+                }
             });
         }
     }
