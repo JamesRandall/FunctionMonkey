3964aff6b743b7f914538ba274071338d302d678
James Randall <james@accidentalfish.com>
2018-12-05 11:52:06 +0000

    Resolved cosmos creation issue

--- Tests/FunctionMonkey.Tests.Integration.Provisioning/deploy.sh
+++ Tests/FunctionMonkey.Tests.Integration.Provisioning/deploy.sh
@@ -1,7 +1,7 @@
-az group create -l westeurope -n functionMonkeyIntegration
+# az group create -l westeurope -n functionMonkeyIntegration
+# az group deployment create -g functionMonkeyIntegration --template-file azuredeploy.json
 
-az group deployment create -g functionMonkeyIntegration --template-file azuredeploy.json
-
+echo "** Creating Storage Containers"
 STORAGEACCOUNTNAME=`az storage account list --resource-group functionMonkeyIntegration --query '[0].name'`
 STORAGEACCOUNTNAME="${STORAGEACCOUNTNAME%\"}"
 STORAGEACCOUNTNAME="${STORAGEACCOUNTNAME#\"}"
@@ -18,6 +18,14 @@ COSMOSDBNAME="${COSMOSDBNAME#\"}"
 COSMOSDBKEY=`az cosmosdb list-keys --name $COSMOSDBNAME --resource-group functionMonkeyIntegration --query primaryMasterKey`
 COSMOSDBKEY="${COSMOSDBKEY%\"}"
 COSMOSDBKEY="${COSMOSDBKEY#\"}"
-az cosmosdb database create --db-name cosmosDatabase --key "$COSMOSDBKEY" --name "$COSMOSDBNAME" --resource-group-name functionMonkeyIntegration
-az cosmosdb collection create --db-name cosmosDatabase --collection-name cosmosCollection --key "$COSMOSDBKEY" --name "$COSMOSDBNAME" --resource-group-name functionMonkeyIntegration --throughput 400 --partition-key-path "/id"
 
+COSMOSDATABASEEXISTS=`az cosmosdb database exists --db-name cosmosDatabase --key "$COSMOSDBKEY" --name "$COSMOSDBNAME" --resource-group-name functionMonkeyIntegration`
+if [[ "$COSMOSDATABASEEXISTS" == "false" ]]; then
+    echo "** CREATING COSMOS DATABASE"
+    az cosmosdb database create --db-name cosmosDatabase --key "$COSMOSDBKEY" --name "$COSMOSDBNAME" --resource-group-name functionMonkeyIntegration
+fi
+COSMOSCOLLECTIONEXISTS=`az cosmosdb collection exists --db-name cosmosDatabase --collection-name cosmosCollection --key "$COSMOSDBKEY" --name "$COSMOSDBNAME" --resource-group-name functionMonkeyIntegration`
+if [[ "$COSMOSCOLLECTIONEXISTS" == "false" ]]; then
+    echo "** CREATING COSMOS COLLECTION"
+    az cosmosdb collection create --db-name cosmosDatabase --collection-name cosmosCollection --key "$COSMOSDBKEY" --name "$COSMOSDBNAME" --resource-group-name functionMonkeyIntegration --throughput 400 --partition-key-path "/id"
+fi
