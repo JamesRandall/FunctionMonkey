4e3dfe29dcb2e806d1fe60b1f751aa846a4183f2
James Randall <james@accidentalfish.com>
2019-03-23 11:11:15 +0000

    Cosmos output binding integration test

---
+++ Tests/FunctionMonkey.Tests.Integration/Common/CosmosMarker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace FunctionMonkey.Tests.Integration.Common
+{
+    public class CosmosMarker
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        public Guid MarkerId { get; set; }
+    }
+}
