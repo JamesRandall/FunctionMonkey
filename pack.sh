41f0d4fd875a8189db5ab8a80bf2c441c181a3d0
James Randall <james@accidentalfish.com>
2019-10-05 13:35:48 +0100

    Changed relative paths in pack

--- pack.sh
+++ pack.sh
@@ -1,10 +1,10 @@
 
-dotnet pack ./Source/FunctionMonkey/FunctionMonkey.csproj --output ../../ --configuration Release
-dotnet pack ./Source/FunctionMonkey.Abstractions/FunctionMonkey.Abstractions.csproj --output ../../ --configuration Release
-dotnet pack ./Source/FunctionMonkey.Commanding.Abstractions/FunctionMonkey.Commanding.Abstractions.csproj --output ../../ --configuration Release
-dotnet pack ./Source/FunctionMonkey.Commanding.Cosmos.Abstractions/FunctionMonkey.Commanding.Cosmos.Abstractions.csproj --output ../../ --configuration Release
-dotnet pack ./Source/FunctionMonkey.Compiler/FunctionMonkey.Compiler.csproj --output ../../ --configuration Release
-dotnet pack ./Source/FunctionMonkey.FluentValidation/FunctionMonkey.FluentValidation.csproj --output ../../ --configuration Release
-dotnet pack ./Source/FunctionMonkey.SignalR/FunctionMonkey.SignalR.csproj --output ../../ --configuration Release
-dotnet pack ./Source/FunctionMonkey.Testing/FunctionMonkey.Testing.csproj --output ../../ --configuration Release
-dotnet pack ./Source/FunctionMonkey.TokenValidator/FunctionMonkey.TokenValidator.csproj --output ../../ --configuration Release
+dotnet pack ./Source/FunctionMonkey/FunctionMonkey.csproj --output ./ --configuration Release
+dotnet pack ./Source/FunctionMonkey.Abstractions/FunctionMonkey.Abstractions.csproj --output ./ --configuration Release
+dotnet pack ./Source/FunctionMonkey.Commanding.Abstractions/FunctionMonkey.Commanding.Abstractions.csproj --output ./ --configuration Release
+dotnet pack ./Source/FunctionMonkey.Commanding.Cosmos.Abstractions/FunctionMonkey.Commanding.Cosmos.Abstractions.csproj --output ./ --configuration Release
+dotnet pack ./Source/FunctionMonkey.Compiler/FunctionMonkey.Compiler.csproj --output ./ --configuration Release
+dotnet pack ./Source/FunctionMonkey.FluentValidation/FunctionMonkey.FluentValidation.csproj --output ./ --configuration Release
+dotnet pack ./Source/FunctionMonkey.SignalR/FunctionMonkey.SignalR.csproj --output ./ --configuration Release
+dotnet pack ./Source/FunctionMonkey.Testing/FunctionMonkey.Testing.csproj --output ./ --configuration Release
+dotnet pack ./Source/FunctionMonkey.TokenValidator/FunctionMonkey.TokenValidator.csproj --output ./ --configuration Release
