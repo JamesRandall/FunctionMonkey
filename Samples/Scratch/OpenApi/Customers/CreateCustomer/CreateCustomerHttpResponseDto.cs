6f937875fd3832b09dcfbed49214e81b626c0ff3
Markus Bernhardt <markus.bernhardt@b4payment.de>
2019-10-24 19:12:08 +0200

    Adding OpenAPI support for DataContract and DataMember attributes

---
+++ Samples/Scratch/OpenApi/Customers/CreateCustomer/CreateCustomerHttpResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace OpenApi.Customers.CreateCustomer
+{
+    /// <summary>
+    ///     The response DTO for the CreateCustomer request
+    /// </summary>
+    [DataContract(Name = "CreateCustomerHttpResponse")]
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class CreateCustomerHttpResponseDto
+    {
+        /// <summary>
+        ///     The customer.
+        /// </summary>
+        [DataMember(Name = "customer")]
+        public CustomerDto Customer { get; set; }
+    }
+}
---
+++ Samples/Scratch/OpenApi/FunctionAppConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using FunctionMonkey.Abstractions;
+using FunctionMonkey.Abstractions.Builders;
+using OpenApi.Customers;
+
+namespace OpenApi
+{
+    public class FunctionAppConfiguration : IFunctionAppConfiguration
+    {
+        public void Build(IFunctionHostBuilder builder)
+        {
+            builder
+                .Setup((serviceCollection, commandRegistry) =>
+                {
+                    commandRegistry.Discover(typeof(FunctionAppConfiguration).Assembly);
+                })
+                .OpenApiEndpoint(openApi => openApi
+                    .Title("My API Title")
+                    .Version("1.2.3")
+                    .UserInterface()
+                )
+                .Functions(functions =>
+                {
+                    functions.RegisterCustomers();
+                });
+
+        }
+    }
+}
